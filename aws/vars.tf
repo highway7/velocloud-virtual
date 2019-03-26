f228e4c8ffc6570b27a7ab50c055e97e8c605e36
Craig Bruenderman <craig@bruenderman.org>
2019-03-26 14:37:40 -0400

    Touch acivation

--- aws/vars.tf
+++ aws/vars.tf
@@ -1,22 +1,21 @@
 variable "velocloud_port" {
   description = "The port the edge will use"
-  default = 2426
+  default     = 2426
 }
 
 variable "velocloud_activation_code" {
   description = "Activation code from the portal"
-  default = "JE87-MYZL-VH6Y-CWWR"
+  default     = "L2S8-UTSJ-Z89L-HS2J"
 }
 
 variable "vco_hostname" {
-  default = "vco160-usca1.velocloud.net"
+  default     = "vco160-usca1.velocloud.net"
   description = "VCO this edge is associated with"
 }
 
 variable "vpc_cidr_block" {
   default = "10.50.0.0/16"
 }
-
 
 variable "public_cidr" {
   default = "10.50.0.0/24"
